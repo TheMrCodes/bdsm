13821e8d8ac7f8d47fe5f6d9e1727a51572c07ab
adrowitzer <alexander@adrowitzer.at>
2021-06-09 11:42:33 +0000

    fix locale error

--- bdsm/tools/dataprep.py
+++ bdsm/tools/dataprep.py
@@ -5,7 +5,6 @@ functions to inspect and prepare a dataset.
 
 # Author: Alexander Adrowitzer <alexander.adrowitzer@fhstp.ac.at>
 
-import locale
 import pandas as pd
 
 def quality(df):
@@ -20,12 +19,12 @@ def quality(df):
                 number of missing values per column (absolute and relative)
                 number of unique values per column
         """   
-    locale.setlocale(locale.LC_ALL, 'en_US')
     result = pd.DataFrame(df.dtypes,columns=['type'])
     result['unique'] = pd.DataFrame(df.nunique())
     result['missing_abs'] = pd.DataFrame(df.isna().sum())
     result['missing_rel'] = pd.DataFrame(round(100*df.isna().sum()/len(df),2))
-    print("Dataframe has " + locale.format_string("%d", df.shape[0], grouping=True) + " rows and " + str(df.shape[1]) + " columns.\n")
+    numrows = df.shape[0]
+    print("Dataframe has " + f'{numrows:,}' + " rows and " + str(df.shape[1]) + " columns.\n")
     print(str(result[result['missing_abs'] != 0].shape[0]) + " columns have missing values.")
     
     return result
